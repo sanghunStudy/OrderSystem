55365d5b11ae6af4d9f84b1fb83786291989add1
kim <kim@kim-PC.mshome.net>
2019-03-05 18:30:19 +0900

    실험

---
+++ OrderSystem/src/main/java/kr/ac/kopo/model/ReviewBoardComment.java
@@ -0,0 +1,45 @@
+package kr.ac.kopo.model;
+
+import java.util.Date;
+
+public class ReviewBoardComment {
+	private int rcommentId;
+	private String id;
+	private String rcommentContent;
+	private String rcommentDate;
+	private int reviewId;
+	
+	
+	public int getRcommentId() {
+		return rcommentId;
+	}
+	public void setRcommentId(int rcommentId) {
+		this.rcommentId = rcommentId;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getRcommentContent() {
+		return rcommentContent;
+	}
+	public void setRcommentContent(String rcommentContent) {
+		this.rcommentContent = rcommentContent;
+	}
+	public String getRcommentDate() {
+		return rcommentDate;
+	}
+	public void setRcommentDate(String rcommentDate) {
+		this.rcommentDate = rcommentDate;
+	}
+	public int getReviewId() {
+		return reviewId;
+	}
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+	
+	
+}

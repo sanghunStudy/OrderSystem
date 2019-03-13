4c779188fa28649e1c15fe21127499bd965bdac9
Student <Student@DESKTOP-EF9PEQ2>
2019-03-13 19:54:09 +0900

    크롤링 테스트중

--- OrderSystem/src/main/java/kr/ac/kopo/controller/getLocationController.java
+++ OrderSystem/src/main/java/kr/ac/kopo/controller/getLocationController.java
@@ -1,7 +1,20 @@
 package kr.ac.kopo.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -13,5 +26,42 @@ public class getLocationController {
 	@RequestMapping("/getLocation")
 	String getLocation() {
 		return path+"getLocation";
+	}
+	
+	@RequestMapping("/test")
+	String test(String data,Model model){
+		System.out.println(data+"<<<<<검색어");
+//		%ED%97%AC%EC%8A%A4%EC%9E%A5
+		String seach = data;
+		String url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query="+seach;
+		Document doc = null;
+		String item = "";
+			try {
+				doc = Jsoup.connect(url).get();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(url);
+
+		
+			if(data.equals("식당")) {
+				Elements element = doc.select(".list_area");
+				item = element.toString();
+				
+			}else if(data.equals("헬스장")) {
+				Elements element = doc.select(".detail");
+				item = element.toString();
+			}
+		
+		
+		
+		System.out.println(item);
+		model.addAttribute("item",item);
+		/*
+		 * Map<String, Object> map = new HashMap<String, Object>();
+		 * map.put("item",item);
+		 */	
+		return path+"getLocation"; 
 	}
 }

2fc070524a03aee710cffc2d9d489d5c8c5c1502
SWY <SWY@DESKTOP-NQ39FVO>
2019-04-30 21:17:00 +0900

    charts testing...

---
+++ OrderSystem/src/main/webapp/resources/js/charts.js
@@ -0,0 +1,63 @@
+//$(function() {
+//	function lineChart(){
+//
+//		var data = {
+//
+//		        labels: ["월","화","수","목","금","토","일"],
+//
+//		        datasets: [
+//
+//		            {
+//
+//		                label: "",
+//
+//		                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+//
+//			            borderColor: 'rgba(54, 162, 235, 1)',
+//
+//		                pointColor: "rgba(220,220,220,1)",
+//
+//		                pointStrokeColor: "#fff",
+//
+//		                pointHighlightFill: "#fff",
+//
+//		                pointHighlightStroke: "rgba(220,220,220,1)",
+//
+//		                data: [2, 3, 5, 7, 11, 13, 17]
+//
+//		            },
+//
+//		            {
+//
+//		                label: "",
+//
+//		                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+//
+//			            borderColor: 'rgba(54, 162, 235, 1)',
+//
+//		                pointColor: "rgba(151,187,205,1)",
+//
+//		                pointStrokeColor: "#fff",
+//
+//		                pointHighlightFill: "#fff",
+//
+//		                pointHighlightStroke: "rgba(151,187,205,1)",
+//
+//		                data: [0, 1, 1, 2, 3, 5, 8]
+//
+//		            }
+//
+//		        ]
+//
+//		    };
+//
+//		    var ctx = document.getElementById("lineCanvas").getContext("2d");
+//
+//		    var options = { };
+//
+//		    var lineChart = new Chart(ctx).Line(data, options);
+//
+//
+//
+//	}
+//});

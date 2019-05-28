776bf87676e34db476a0cc5f2e926987498142e3
BaeSangHun <art9000@naver.com>
2019-05-28 15:23:06 +0900

    댓글에 html태그 입력하면 출력시 해당 태그가 실행되는 버그 고침

--- OrderSystem/src/main/java/kr/ac/kopo/controller/RoutineController.java
+++ OrderSystem/src/main/java/kr/ac/kopo/controller/RoutineController.java
@@ -74,7 +74,7 @@ public class RoutineController {
 		searchVO.pageCalculate(service.totalCount(searchVO));
 
 		List<Routine> list = service.list(searchVO);
-
+		
 		model.addAttribute("list", list);
 		model.addAttribute("SearchVO", searchVO);
 		return path + "list";
@@ -391,7 +391,7 @@ public class RoutineController {
 			for (int i = 0; i < commentVO.size(); i++) {
 				HashMap hm = new HashMap();
 				hm.put("c_code", commentVO.get(i).getNcomentId());
-				hm.put("comment", commentVO.get(i).getNcomentContent());
+				hm.put("comment", commentVO.get(i).getNcomentContent().replaceAll("<(/)?([a-zA-z]*)(\\s[a-zA-z]*=[^>]*)?(\\s)*(/)?>",""));//html 태그 지우는 정규식
 				hm.put("writer", commentVO.get(i).getId());
 				hm.put("NcomentDate", commentVO.get(i).getNcomentDate());
 
